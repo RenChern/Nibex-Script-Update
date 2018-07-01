d9dde09706441f62313db3ad672d3c4a0cc4ea5f
CryptoNeverSleeps <36324479+CryptoNeverSleeps@users.noreply.github.com>
2018-07-01 07:26:35 -0400

    Nibex Update v2.1

---
+++ nibex-update.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+cd ~
+echo "****************************************************************************"
+echo "****************************************************************************"
+echo "*****************************CryptoNeverSleeps******************************"
+
+cd /tmp
+
+nibex-cli -daemon stop
+systemctl stop Nibex.service
+rm /usr/local/bin/nibexd
+rm /usr/local/bin/nibex-cli
+wget -N https://github.com/CryptoNeverSleeps/nibex/releases/download/v2.1/nibex-linux-mn.tar.gz
+tar xvzf nibex-linux-mn.tar.gz --strip 1
+mv nibexd nibex-cli /usr/local/bin
+
+systemctl start Nibex.service
+
+echo "Running nibex-cli getinfo"
+
+nibex-cli getinfo
+
+Echo "Finished"
+
+
